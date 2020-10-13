e5239ffaaf626100382a86de3956119ecee46ea8
Liwei Wang <livi@liweis-mbp.lan>
2020-10-13 18:37:45 -0400

    Fix the scanD, going to remvove the redundant

--- MineApriori.py
+++ MineApriori.py
@@ -14,7 +14,7 @@ def createC1(dataSet):
     return [frozenset(i) for i in C1]
 
 
-#Scan through datasets
+#Scan through datasets, remove the ones below min support
 def scanD(D, Ck, minSupport):
     # Generate counts for the C
     ssCnt = {}
@@ -32,10 +32,12 @@ def scanD(D, Ck, minSupport):
     supportData = {}
     for key in ssCnt:
         support = ssCnt[key]/numItems
-        #print('support',support)
+        print('support',support,'min_support',minSupport,'Bool',support >= minSupport)
         if support >= minSupport:
             retList.insert(0, key)
-        supportData[key] = support
+            supportData[key] = support
+    print('supportData:',pd.DataFrame.from_dict(supportData,orient='index'))
+    print('-'*30)
     return retList, supportData
 
 
@@ -63,20 +65,20 @@ def apriori(dataSet, minSupport=0.5):
     D=list(map(set,dataSet))
     L1, supportData = scanD(D, C1, minSupport)
 
-    print('supportData',pd.DataFrame(supportData.values(),supportData.keys()))
-    print('L1',(L1))
-    print('------Seed------'*5,'\n')
+    #print('supportData',pd.DataFrame(supportData.values(),supportData.keys()))
+    #print('L1',(L1))
+    #print('------Seed------'*5,'\n')
 
     #repeatively generate L and C
     L = [L1]
     k = 2 #help to put values in L
     while (len(L[k-2]) > 0):
-        print('L_',k-2,L[k-2])
+        #print('L_',k-2,L[k-2])
         Ck = aprioriGen(L[k-2], k) 
-        print(pd.DataFrame({'Ck':Ck}))
+        #print(pd.DataFrame({'Ck':Ck}))
         Lk, supK = scanD(D, Ck, minSupport) 
         supportData.update(supK)
-        print('*'*10)
+        #print('*'*10)
         if len(Lk) == 0:
             break
         # all the Lk generated along the way are put into L.
